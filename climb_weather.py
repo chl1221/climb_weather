6fd8bdda7158e05859eacedff6fb84f4bf1217bf
Ian Li <63522673+chl1221@users.noreply.github.com>
2022-02-24 13:44:22 -0800

    Update climb_weather.py

--- climb_weather.py
+++ climb_weather.py
@@ -17,8 +17,8 @@ def get_geo_info() -> tuple:
         response = requests.get(url=POSITIONSTACK_ENDPOINT, params=parameters)
         response.raise_for_status()
         geo_data = response.json()["data"][0]
-        return geo_data["latitude"], geo_data["longitude"], geo_data["region_code"]
-    except requests.exceptions.HTTPError:
+        return geo_data["latitude"], geo_data["longitude"]
+    except requests.exceptions.RequestException:
         print(f"Error! {response.json()['error']['message']}.")
     
 def distance_cal(lat_a: float, lon_a: float, lat_b: float, lon_b: float) -> int:
@@ -30,8 +30,9 @@ def distance_cal(lat_a: float, lon_a: float, lat_b: float, lon_b: float) -> int:
 
 def get_rc_spots() -> list:
     """return a heap containing rockclimbing spots within 150 miles."""
-    try:
-        lat, lon, state = get_geo_info()
+    geo_info = get_geo_info()
+    if geo_info:
+        lat, lon = geo_info
         CLIMBINGWEATHER_ENDPOINT = "https://api.climbingweather.com/country"
         response = requests.get(url=f"{CLIMBINGWEATHER_ENDPOINT}/USA/area")
         response.raise_for_status()
@@ -40,13 +41,12 @@ def get_rc_spots() -> list:
         near_spots_heap = []
         for spot in data:
             # some data do not have latitude and longitude
-            if spot["latitude"] != "None" or spot["longitude"] != "None":
-                distance = round(distance_cal(lat, lon, float(spot['latitude']), float(spot['longitude'])), 2)
+            if spot["latitude"] != "None" and spot["longitude"] != "None":
+                distance = round(distance_cal(lat, lon, float(spot["latitude"]), float(spot["longitude"])), 2)
                 if distance <= 150:
-                    heapq.heappush(near_spots_heap, (distance, spot['areaId'], spot['name']))
+                    # print(spot)
+                    heapq.heappush(near_spots_heap, (distance, spot["areaId"], spot["name"], spot["adminArea"]))
         return near_spots_heap
-    except TypeError:
-        print("Error! Did not get the valid Geocode.")
 
 def get_forecast() -> None:
     """print a list of rockclimbig spots with nice weather for the weekend."""
@@ -57,22 +57,22 @@ def get_forecast() -> None:
     CLIMBINGWEATHER_ENDPOINT = "https://api.climbingweather.com/area"
     # for_check = []
     while len(plan) < 5 and near_spots:
-        distance, area_ID, area_name = heapq.heappop(near_spots)
+        distance, area_ID, area_name, state = heapq.heappop(near_spots)
         response = requests.get(url=f"{CLIMBINGWEATHER_ENDPOINT}/{area_ID}/forecast")
         response.raise_for_status()
         data = response.json()["daily"]["data"]
         for day in data:
             date = dt.datetime.fromtimestamp(day['time'])
             if date.weekday() >= 5:
-                if day["temperatureHigh"] <= 82 and day["temperatureLow"] >= 65:
+                if day["temperatureHigh"] <= 82 and day["temperatureLow"] >= 30:
                     if float(day['precipProbability']) <= 0.1:
                         if day["windSpeed"] <= 6:
                             # for_check.append(day)
-                            plan.append((date, distance, area_name))
+                            plan.append((date, distance, area_name, state))
     # print(for_check)
     if len(plan) > 0:
         for spot in plan:
-            print(f"{spot[2]} (distance: {spot[1]} miles) is good on {spot[0].strftime('%b-%d (%a)')}")
+            print(f"{spot[2]}, {spot[3]} (distance: {spot[1]} miles) is good on {spot[0].strftime('%b-%d (%a)')}")
     else:
         print("It's not a good time to climb outdoors.")
 
